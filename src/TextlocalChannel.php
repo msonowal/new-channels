85b139036da0fd5f17c7a7a68e70a60480be9995
Shankar Akunuri <bhvnshnkr3@gmail.com>
2019-12-09 21:28:22 +0530

    Update TextlocalChannel.php
    Set sender ID in notification.

--- src/TextlocalChannel.php
+++ src/TextlocalChannel.php
@@ -56,6 +56,10 @@ class TextlocalChannel
             $unicode = $notification->getUnicodeMode();
         }
 
+        if (method_exists($notification, 'getSenderId')) {
+            $this->sender = $notification->getSenderId();
+        }
+
         try {
             $response = $this->client->setUnicodeMode($unicode)
             ->sendSms($numbers, $message, $this->sender);
